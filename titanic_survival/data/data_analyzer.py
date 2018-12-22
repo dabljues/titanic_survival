7925306fc43dffb6b7a75317b744f83b10139b5f
dabljues <dabljues96@gmail.com>
2018-12-23 00:34:02 +0100

    Added barplots order arg handling, which has changed pclass_sex graph appearance

--- titanic_survival/data/data_analyzer.py
+++ titanic_survival/data/data_analyzer.py
@@ -26,7 +26,8 @@ class DataAnalyzer:
         return ClassificationData(self.train_data.drop(features, axis=1), self.train_data[features],
                                   self.test_data.drop('PassengerId', axis=1).copy())
 
-    def _facet_grid(self, func, x: List[str], col: str = None, row: str = None, orient: str = 'v',
+    def _facet_grid(self, func, x: List[str], order: List[str] = None, col: str = None, row: str = None,
+                    orient: str = 'v',
                     color: str = None, save_name: 'str' = None, show: bool = False) -> None:
         # Enable/disable showing this particular plot
         if not show:
@@ -35,7 +36,7 @@ class DataAnalyzer:
             plt.ion()
         g = sns.FacetGrid(self.train_data, col=col, row=row)
         if func == sns.barplot:
-            g.map(func, *x, orient=orient, ci=None, color=color)
+            g.map(func, *x, order=order, orient=orient, ci=None, color=color)
         else:
             g.map(func, *x, color=color)
         if save_name is not None:
@@ -45,8 +46,9 @@ class DataAnalyzer:
         # Check if survival rate is connected with Age
         self._facet_grid(plt.hist, col='Survived', x=['Age'], show=True, save_name='age_survival')
         # Check if survival rate is connected with Sex
-        self._facet_grid(sns.barplot, col='Sex', x=['Survived'], color='#ff9933', save_name='sex_survival.png')
+        self._facet_grid(sns.barplot, col='Sex', x=['Survived'], color='#ff9933',
+                         save_name='sex_survival.png')
         # Check if the survival rate is connected with PClass
         self._facet_grid(sns.barplot, col='Pclass', x=['Survived'], color='#00ff00', save_name='pclass_survival.png')
-        self._facet_grid(sns.barplot, col='Pclass', row='Sex', x=['Survived'], color='#ff33cc',
+        self._facet_grid(sns.barplot, col='Pclass', x=['Sex', 'Survived'], order=['female', 'male'], color='#ff33cc',
                          save_name='pclass_sex_survival.png')
