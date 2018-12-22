56e1f83fa04519a031082c368fe77cdc01daa5a0
dabljues <dabljues96@gmail.com>
2018-12-22 23:56:59 +0100

    Added some colors to the graphs. Added a possibility to enable/disable graph showing and another to save graphs as images

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ self._facet_grid(plt.hist, col='Survived', x=['Age'])
 ```
 Using `DataAnalyzer` class, we print this graph which shows correlation between Age and survival rate (very young and very old passengers) have better chance to survive than passengers in age `15-45`:
 
-![alt text](https://i.imgur.com/UkkawqA.png "")
+![alt text](images/age_survival.png "")
 ## 2. Sex
 As many would think - sex is correlated with survival rate - females had a better chance to survive.
 ```python
@@ -17,20 +17,23 @@ self._facet_grid(sns.barplot, col='Sex', row='Pclass', x=['Survived'])
 ```
 This plot shows us how the sex of the passengers affected their survival chance:
 
-![alt text](https://i.imgur.com/NpaeW0k.png "")
+![alt text](images/sex_survival.png "")
 
 As we can see - women were 3/4 times more likely to survive the disaster than men. Knowing that, we will include `Sex` feature.
 ## 3. Pclass
 Naturally we would think, that the class of travel would have an impact on the survivability chance (at least back then). This figure shows us the percentage of survival by `Pclass`.
 
-![alt text](https://i.imgur.com/GdKJ2xt.png "")
+![alt text](images/pclass_survival.png "")
 
 That definitely shows us that `Pclass` feature is worth taking into consideration.
 
 Interesting thing happens when we cross-compare those results with `Sex` feature:
 
-![alt text](https://i.imgur.com/c4XwF42.png "")
+![alt text](images/pclass_sex_survival.png "")
 
 As we can see - the sex of the passengers matters, but its effect on survival chance varies between different `Pclasses`. That's another argument to include `Pclass` feature in our further computation.
 ## 4. Fare
+The next hypothesis is: who paid more had a better chance to survive. This graph shows the effect of `Fare` feature on survivability:
+
+
 ## 5. Embarked
--- titanic_survival/data/data_analyzer.py
+++ titanic_survival/data/data_analyzer.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import dataclass
 from typing import List
 
@@ -25,20 +26,27 @@ class DataAnalyzer:
         return ClassificationData(self.train_data.drop(features, axis=1), self.train_data[features],
                                   self.test_data.drop('PassengerId', axis=1).copy())
 
-    def _facet_grid(self, func, x: List[str], col: str = None, row: str = None, orient: str = 'v') -> None:
+    def _facet_grid(self, func, x: List[str], col: str = None, row: str = None, orient: str = 'v',
+                    color: str = None, save_name: 'str' = None, show: bool = False) -> None:
+        # Enable/disable showing this particular plot
+        if not show:
+            plt.ioff()
+        else:
+            plt.ion()
         g = sns.FacetGrid(self.train_data, col=col, row=row)
         if func == sns.barplot:
-            g.map(func, *x, orient=orient, ci=None)
+            g.map(func, *x, orient=orient, ci=None, color=color)
         else:
-            g.map(func, *x)
-        g.add_legend()
-        plt.show()
+            g.map(func, *x, color=color)
+        if save_name is not None:
+            plt.savefig(os.path.abspath(f'../images/{save_name}'), bbox_inches='tight')
 
     def analyze(self) -> None:
         # Check if survival rate is connected with Age
-        self._facet_grid(plt.hist, col='Survived', x=['Age'])
+        self._facet_grid(plt.hist, col='Survived', x=['Age'], show=True, save_name='age_survival')
         # Check if survival rate is connected with Sex
-        self._facet_grid(sns.barplot, col='Sex', x=['Survived'])
+        self._facet_grid(sns.barplot, col='Sex', x=['Survived'], color='#ff9933', save_name='sex_survival.png')
         # Check if the survival rate is connected with PClass
-        self._facet_grid(sns.barplot, col='Pclass', x=['Survived'])
-        self._facet_grid(sns.barplot, col='Pclass', row='Sex', x=['Survived'])
+        self._facet_grid(sns.barplot, col='Pclass', x=['Survived'], color='#00ff00', save_name='pclass_survival.png')
+        self._facet_grid(sns.barplot, col='Pclass', row='Sex', x=['Survived'], color='#ff33cc',
+                         save_name='pclass_sex_survival.png')
